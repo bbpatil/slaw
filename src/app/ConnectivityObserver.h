613dd9e0737cf044cb0cfca797b57b7d6fc8405b
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2019-10-08 20:36:19 -0500

    Describe both the purpose and the elements of adjacency_matrix

--- src/app/ConnectivityObserver.h
+++ src/app/ConnectivityObserver.h
@@ -2,14 +2,31 @@
 #define CONNECTIVITY_OBSERVER
 
 #include <vector>
-
+#include <tuple>
 #include <omnetpp.h>
 #include "PositionObserver.h"
 
 class ConnectivityObserver : public PositionObserver {
-  /** @brief A data structure to track links between neighbors */
-  std::vector< std::vector<bool> > adjacency_matrix;
+protected:
+  typedef std::tuple<omnetpp::simtime_t, omnetpp::simtime_t, bool> cell;
+  /** @brief A data structure to track links between neighbors. 
+   *  The matrix keeps triples of kind<link_duration, start, status>. The 
+   *  link_duration element is the overall time a link between two nodes
+   *  was established. The start element is the time a link is established. The 
+   *  status element is a boolean value indicating whether a link is up */
+  std::vector< std::vector<cell> > adjacency_matrix;
+protected:
 
+public:
+  /** @brief Default constructor */
+  ConnectivityObserver();
+  /** @brief Initializes the attributes of this class */
+  virtual void initialize(int stage) override;
+  /** @brief Receives the quadrant of a module and updates its one-hop 
+   * neighborhood */
+  virtual void receiveSignal(omnetpp::cComponent*, omnetpp::simsignal_t,  
+    omnetpp::cObject*, omnetpp::cObject*) override;
+  virtual void finish() override;
 };
 
 #endif // CONNECTIVITY_OBSERVER
